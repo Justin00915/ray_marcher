a8ec865645e8886cae44a96d73af20f98bee0c6d
Justin Walter <Walter1Justin1@gmail.com>
2024-01-26 16:28:38 +0100

    FIxed normals to actually be correct

--- SDL Projekt1/scene_objects.cpp
+++ SDL Projekt1/scene_objects.cpp
@@ -3,7 +3,7 @@
 
 HitInfo Sphere::signed_distance(Vector3 other_pos) {
 	double dist = other_pos.get_dist(pos) - r;
-	Vector3 normal = (pos - other_pos).get_normalized();
+	Vector3 normal = (other_pos - pos).get_normalized();
 	return HitInfo{ dist, normal, col };
 }
 
@@ -11,16 +11,15 @@ HitInfo Box::signed_distance(Vector3 other_pos) {
 	Vector3 centered_pos = (other_pos - pos);
 	double dist = Vector3::max(centered_pos.get_absolute() - radii, Vector3(0, 0, 0)).get_length();
 
-	int sign = -1;
+	int sign = 1;
 	Vector3 shenanigans = centered_pos / radii;
 	if (shenanigans.get_max() < shenanigans.get_abs_max()) {
 		shenanigans = shenanigans * -1;
-		sign = 1;
+		sign = -1;
 	}
-	shenanigans = shenanigans - Vector3(1, 1, 1) * (shenanigans.get_max() - 0.00001);
+	shenanigans = shenanigans - Vector3(1, 1, 1) * (shenanigans.get_max());
 
 	Vector3 normal = Vector3::max(shenanigans, Vector3(0, 0, 0)).get_normalized() * sign;
 
 	return HitInfo{ dist, normal, col };
 }
-
